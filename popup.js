e5fb4c9826f91bf81d5e0e454759eeb3f869c935
fkmurphy <njmdistrisoft@gmail.com>
2020-05-20 16:39:08 -0300

    Fix font-close button and load img mobile

--- popup.js
+++ popup.js
@@ -1,28 +1,27 @@
-// Get the modal
 var popup = document.getElementById('popup');
 
-// Get the image and insert it inside the modal - use its "alt" text as a caption
-var popupImg = document.getElementById("img-popup");
+var popupImg =$('#img-popup')
 
+   
+$(document).ready(function(){
+  if(popupImg.attr("with-img-mobile") !== null  && $(this).width() <= 800  ){
+    fileName =  popupImg.attr('src').split('.').slice(0, -1).join('.')
+    extension = popupImg.attr('src').slice(-4)
+    console.log("es mobil")
+    popupImg.attr('src', fileName+"-mobile"+extension) 
+  } 
+  popup.style.display = "block";
 
-$(window).on('load',function(){
-    popup.style.display = "block";
-    if(popupImg.getAttribute("with-img-mobile") !== null  && $(this).width() <= 800  ){
-      fileName =  popupImg.src.split('.').slice(0, -1).join('.')
-      extension = popupImg.src.slice(-4)
-      console.log("es mobil")
-      popupImg.src = fileName+"-mobile"+extension
-    }
 })
 
-// Get the <span> element that closes the modal
+$(document).load(function(){
+})
+
 var span = document.getElementsByClassName("close")[0];
 
-// When the user clicks on <span> (x), close the modal
 span.onclick = function() { 
   $('#popup').fadeOut(150);
 }
 popup.onclick = function(){
-  //$mod.fadeOut(150);
   $('#popup').fadeOut(150);
 };
